7095b7699215853aca521489c97094e0cffaea89
mlouveaux <marion.louveaux@cos.uni-heidelberg.de>
2018-05-08 16:21:29 +0200

    Moving the function plotlyMesh from the vignette to separate .R file.

---
+++ R/plotlyMesh.R
@@ -0,0 +1,133 @@
+#' A layout for plotly graphs
+#'
+#' This function provides a basic layout for plotly graphs used to display MGX meshes.
+#' @param meshExample mesh loaded with the modified_read.ply function
+#' @param meshColors to display colored on the mesh
+#' @param meshCellcenter to display the center of (biological) cells on the mesh
+#' @importFrom magrittr %>%
+#' @keywords plotly
+#' @export
+#' @examples
+#' @return plotly graph
+#' plotlyMesh()
+
+
+plotlyMesh <- function(meshExample,
+                       meshColors = NULL,
+                       meshCellcenter = NULL){
+  makeColorScale <- FALSE
+  if (!is.null(nrow(meshColors)) && ncol(meshColors)>1){
+    color <- NULL
+    for (i in 1:ncol(meshColors)){
+      color[i] <-  setdiff(unique(meshColors[,i]), "#000000") # here I remove black vertices
+    } # to be more general, remove any color shared by two vertices
+    opacity <- 1
+  }else{
+    if (is.null(meshColors)){
+      color <- rep("#CCCCFF", ncol(myMesh$it)) # "#00FFFF"
+      opacity <- 0.2
+    }else{
+      makeColorScale <- TRUE
+      colorCut <- cut(pull(meshColors), 15,
+                      labels =  colorRamps::matlab.like(15)
+      )
+      color <- as.character(colorCut)
+      opacity <- 1
+    }
+  }
+
+  trace2 <- list(type="mesh3d",
+                 x = meshExample$vb[1,],
+                 y = meshExample$vb[2,],
+                 z = meshExample$vb[3,],
+                 i = meshExample$it[1,]-1, # NB indices start at 0
+                 j = meshExample$it[2,]-1,
+                 k = meshExample$it[3,]-1,
+                 facecolor = color, #one color per triangle (not per biological cell)
+                 opacity = opacity
+  )
+  #facecolor: one color per triangle (e.g. length(facecolor) == length(i))
+
+  if (makeColorScale){
+    trace4 <- list(
+      x = c(100,1,200),
+      y = c(200,1,1),
+      z = c(1,500,3),
+      marker = list(
+        autocolorscale = FALSE,
+        cmax = max(meshColors),#2.5,
+        cmin = min(meshColors),#0,
+        colorbar = list(
+          x = 1.2,
+          y = 0.5,
+          len = 0.3,
+          thickness = 15,
+          tickfont = list(size = 12),
+          titlefont = list(size = 20)
+        ),
+        colorscale = matlab.like(15), # from colorRamps
+        # reversescale = TRUE,
+        line = list(width = 0),
+        opacity = 0.9,
+        showscale = TRUE,
+        size = 20,
+        symbol = "circle"
+      ),
+      mode = "markers",
+      opacity = 0,
+      type = "scatter3d"
+    )
+  }
+
+  layout <- list(
+    scene = list(
+      xaxis = list(
+        backgroundcolor = "rgb(230,230,230)",
+        gridcolor = "rgb(255,255,255)",
+        showbackground = TRUE,
+        zerolinecolor = "rgb(255,255,255)"
+      ),
+      yaxis = list(
+        backgroundcolor = "rgb(230,230,230)",
+        gridcolor = "rgb(255,255,255)",
+        showbackground = TRUE,
+        zerolinecolor = "rgb(255,255,255)"
+      ),
+      zaxis = list(
+        backgroundcolor = "rgb(230,230,230)",
+        gridcolor = "rgb(255,255,255)",
+        showbackground = TRUE,
+        zerolinecolor = "rgb(255,255,255)"
+      )
+    ),
+    title = "My mesh",
+    xaxis = list(title = "m[, 1]"),
+    yaxis = list(title = "m[, 2]")
+  )
+
+  p <- plot_ly()
+  p <- add_trace(p, x = trace2$x, y=trace2$y, z=trace2$z,
+                 facecolor=trace2$facecolor,
+                 i=trace2$i, j=trace2$j, k=trace2$k, type=trace2$type,
+                 opacity=trace2$opacity)
+  if (makeColorScale){
+    p <- add_trace(p, x=trace2$x, y=trace2$y, z=trace2$z,
+                   marker = trace4$marker,
+                   mode = trace4$mode,
+                   opacity = trace4$opacity,
+                   type = trace4$type)
+  }
+  if (!is.null(meshCellcenter)){ # show cell center
+    p <- add_trace(p, x = meshCellcenter$x,
+                   y = meshCellcenter$y,
+                   z = meshCellcenter$z,
+                   type = "scatter3d",
+                   mode = "markers",
+                   marker=list(color = 'rgb(255, 255, 255)',
+                               size = 1.5)
+    )
+  }
+  p <- layout(p, scene=layout$scene, title=layout$title, xaxis=layout$xaxis, yaxis=layout$yaxis)
+
+  p
+}
