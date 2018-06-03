a33ea0eb4b136dc2e7614f2c4aec872a6001d1bd
mlouveaux <marion.louveaux@cos.uni-heidelberg.de>
2018-06-03 17:55:57 +0200

    Filling section of mgx2r-package (main help page).

--- R/mgx2r-package.R
+++ R/mgx2r-package.R
@@ -7,4 +7,17 @@
 #' @aliases mgx2r mgx2r-package
 #' @docType package
 #' @author Marion <marion.louveaux@@gmail.com>
+#'
+#' @section Import from MorphoGraphX (MGX)
+#'
+#' The function read_mgxPly imports meshes created with MorphoGraphX and saved as .ply as 3D mesh objects.
+#' The function read_mgxCellGraph imports cell graphs created with MorphoGraphX and saved as .ply as list of dataframes.
+#'
+#' @section Visualisation and data exploration
+#'
+#' Examples of simple data exploration of variables stored in the cell graphs are provided in the vignettes.
+#'
+#' @section Export to MorphoGraphX (MGX)
+#'
+#' The function mesh2ply exports a 3D mesh object from R to a .ply file that can be opened in MorphoGraphX (MGX).
 NULL
