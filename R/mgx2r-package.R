ffe73009206e67fe9958c67d7ff5b507274378dd
mlouveaux <marion.louveaux@cos.uni-heidelberg.de>
2018-06-03 14:17:40 +0200

    Adding file mgx2r-package.R to have a main help page for the package.

---
+++ R/mgx2r-package.R
@@ -0,0 +1,10 @@
+#' mgx2r-package
+#'
+#' Import mesh and cell graph files from MorphoGraphX software as tidy dataframes.
+#' Propose visualization solutions for mesh and cell graph files from MorphoGraphX with Plotly and Shiny.
+#'
+#' @name mgx2r-package
+#' @aliases mgx2r mgx2r-package
+#' @docType package
+#' @author Marion <marion.louveaux@@gmail.com>
+NULL
