99594be056659edb0deadcdc193974b6c813cdd7
mlouveaux <marion.louveaux@cos.uni-heidelberg.de>
2018-06-10 15:56:01 +0200

    Rebuilding vignettes with snapshots instead of real data (smaller package size).

--- vignettes/vignette_basics.R
+++ vignettes/vignette_basics.R
@@ -4,26 +4,38 @@ knitr::opts_chunk$set(
   comment = "#>"
 )
 
-## ----lib, warning = FALSE, message = FALSE-------------------------------
-library(colorRamps)
-library(mgx2r)
-library(magrittr)
-library(dplyr)
-library(plotly)
-library(glue)
-library(RColorBrewer)
+## ----lib, warning = FALSE, message = FALSE, eval=FALSE-------------------
+#  library(colorRamps)
+#  library(mgx2r)
+#  library(magrittr)
+#  library(dplyr)
+#  library(plotly)
+#  library(glue)
+#  library(RColorBrewer)
+#  
+#  library(cellviz3d)
 
-## ----myData--------------------------------------------------------------
-filePly <- system.file("extdata", "normalMesh/2013-02-12_LTi6B_dis_A_T0_cells_minW1_normalMesh.ply", package = "mgx2r")
+## ----myData, eval=FALSE--------------------------------------------------
+#  # Choose one of the two datasets. Generating plots with the full dataset might be slow.
+#  
+#  ### Full datataset
+#  filePly <- system.file("extdata", "full/normalMesh/2013-02-12_LTi6B_dis_A_T0_cells_minW1_normalMesh.ply", package = "mgx2r")
+#  
+#  fileCellGraph <- system.file("extdata",  "full/cellGraph/2013-02-12_LTi6B_dis_A_T0_cells_minW1_cellGraph.ply", package = "mgx2r")
+#  
+#  
+#  ### Cropped dataset
+#  filePly <- system.file("extdata", "cropped/normalMesh/2013-02-12_LTi6B_dis_A_T12h_cells_minW1_cropped_normalMesh.ply", package = "mgx2r")
+#  
+#  fileCellGraph <- system.file("extdata",  "cropped/cellGraph/2013-02-12_LTi6B_dis_A_T12h_cells_minW1_cropped_cellGraph.ply", package = "mgx2r")
+#  
 
-fileCellGraph <- system.file("extdata",  "cellGraph/2013-02-12_LTi6B_dis_A_T0_cells_minW1_cellGraph.ply", package = "mgx2r")
-
-## ----readPly, warning = FALSE, message = FALSE---------------------------
-myMesh <- read_mgxPly(file = filePly, ShowSpecimen = FALSE, addNormals = TRUE,
-                               MatCol= 1, header_max = 30,
-                               my_colors = c("#800000", "#FF0000", "#808000", "#FFFF00",
-                                             "#008000", "#00FF00", "#008080", "#00FFFF",
-                                             "#000080", "#0000FF", "#800080", "#FF00FF"))
+## ----readPly, warning = FALSE, message = FALSE, eval=FALSE---------------
+#  myMesh <- read_mgxPly(file = filePly, ShowSpecimen = FALSE, addNormals = TRUE,
+#                                 MatCol= 1, header_max = 30,
+#                                 my_colors = c("#800000", "#FF0000", "#808000", "#FFFF00",
+#                                               "#008000", "#00FF00", "#008080", "#00FFFF",
+#                                               "#000080", "#0000FF", "#800080", "#FF00FF"))
 
 ## ---- eval=FALSE---------------------------------------------------------
 #  rgl::shade3d(myMesh)
@@ -32,51 +44,86 @@ myMesh <- read_mgxPly(file = filePly, ShowSpecimen = FALSE, addNormals = TRUE,
 #  myMesh$material$color <- myMesh$allColors$Col_signal
 #  rgl::shade3d(myMesh)
 
-## ----readCellGraph-------------------------------------------------------
-myCellGraph <- read_mgxCellGraph(fileCellGraph = fileCellGraph, header_max = 30)
+## ----readCellGraph, eval=FALSE-------------------------------------------
+#  myCellGraph <- read_mgxCellGraph(fileCellGraph = fileCellGraph_big, header_max = 30)
 
-## ------------------------------------------------------------------------
-meshCellcenter <- myCellGraph$vertices[,c("label","x", "y", "z")]
+## ---- eval=FALSE---------------------------------------------------------
+#  meshCellcenter <- myCellGraph$vertices[,c("label","x", "y", "z")]
+#  
+#  # An other way to define the cell centers without using the cell graph would be:
+#  vertexCellcenter <- purrr::map(1:ncol(myMesh$allColors$Col_label), ~
+#    myMesh$it[ which(myMesh$allColors$Col_label[,.x] == names(which(table(myMesh$allColors$Col_label[,.x]) == 1))), .x ]
+#  )
 
-#other way to define cell centers, without the cell graph
-vertexCellcenter <- purrr::map(1:ncol(myMesh$allColors$Col_label), ~ 
-  myMesh$it[ which(myMesh$allColors$Col_label[,.x] == names(which(table(myMesh$allColors$Col_label[,.x]) == 1))), .x ]
-)
+## ----map3D, eval=FALSE---------------------------------------------------
+#  p1 <- plotlyMesh(meshExample = myMesh,
+#                   meshColors = myMesh$allColors$Col_label,
+#                   meshCellcenter = meshCellcenter) %>%
+#    layout(scene =
+#             list(
+#               aspectmode = "data"
+#             ))
+#  
+#  p1
 
-## ----map3D---------------------------------------------------------------
-p1 <- plotlyMesh(meshExample = myMesh,
-           meshColors = myMesh$allColors$Col_label,
-           meshCellcenter = meshCellcenter)
+## ----echo=FALSE----------------------------------------------------------
+knitr::include_graphics(system.file("img", "full/p1labels.png", package = "mgx2r"))
 
-p1
+## ----map3D2, eval=FALSE--------------------------------------------------
+#  plotlyMesh(meshExample = myMesh,
+#             meshColors = NULL,
+#             meshCellcenter = meshCellcenter)%>%
+#    layout(scene =
+#             list(
+#               aspectmode = "data"
+#             ))
 
-## ----map3D2--------------------------------------------------------------
-plotlyMesh(meshExample = myMesh,
-           meshColors = NULL,
-           meshCellcenter = meshCellcenter)
+## ----echo=FALSE----------------------------------------------------------
+knitr::include_graphics(system.file("img", "full/p1withoutlabels.png", package = "mgx2r"))
 
-## ----heatmap3D, warning = FALSE, message = FALSE-------------------------
-p2 <- plotlyMesh(meshExample = myMesh, 
-           meshColors = left_join(myMesh$it_label, myCellGraph$vertices) %>%
-             select(., GeometryArea),
-           meshCellcenter =  meshCellcenter
-)
-p2
+## ----heatmap3D, warning = FALSE, message = FALSE, eval=FALSE-------------
+#  p2 <- plotlyMesh(meshExample = myMesh,
+#             meshColors = left_join(myMesh$it_label, myCellGraph$vertices) %>%
+#               select(., GeometryArea),
+#             meshCellcenter =  meshCellcenter
+#  ) %>%
+#    layout(scene =
+#             list(
+#               aspectmode = "data"
+#             ))
+#  p2
 
-## ----heatmap3D2, warning = FALSE, message = FALSE------------------------
-meshPlotly <- plotlyMesh(meshExample = myMesh, 
-                meshColors = NULL,
-                meshCellcenter = meshCellcenter)
+## ----echo=FALSE----------------------------------------------------------
+knitr::include_graphics(system.file("img", "full/p2heatmap.png", package = "mgx2r"))
 
-show_tensors(p = meshPlotly,
-             cellGraph = myCellGraph,
-             tensor_name = "CellAxisPDG",
-             scale = 150)
+## ----tensors, warning = FALSE, message = FALSE, eval=FALSE---------------
+#  meshPlotly <- plotlyMesh(meshExample = myMesh,
+#                  meshColors = NULL,
+#                  meshCellcenter = meshCellcenter) %>%
+#    layout(scene =
+#             list(
+#               aspectmode = "data"
+#             ))
+#  
+#  show_tensors(p = meshPlotly,
+#               cellGraph = myCellGraph,
+#               tensor_name = "CellAxiscurv10microns",
+#               scale = 50)
 
-## ----cellContours--------------------------------------------------------
-meshPlotly <- plotlyMesh(meshExample = myMesh,
-           meshColors = NULL)
+## ----echo=FALSE----------------------------------------------------------
+knitr::include_graphics(system.file("img", "full/tensors.png", package = "mgx2r"))
 
-show_cellcontour(p = meshPlotly, mesh = myMesh)
+## ----cellContours, eval=FALSE--------------------------------------------
+#  meshPlotly <- plotlyMesh(meshExample = myMesh,
+#             meshColors = NULL) %>%
+#    layout(scene =
+#             list(
+#               aspectmode = "data"
+#             ))
+#  
+#  show_cellcontour(p = meshPlotly, mesh = myMesh)
+#  
 
+## ----echo=FALSE----------------------------------------------------------
+knitr::include_graphics(system.file("img", "full/contour.png", package = "mgx2r"))
 
--- vignettes/vignette_time_series.R
+++ vignettes/vignette_time_series.R
@@ -4,30 +4,35 @@ knitr::opts_chunk$set(
   comment = "#>"
 )
 
-## ---- warning = FALSE, message = FALSE-----------------------------------
-library(colorRamps)
-library(dplyr)
-library(plotly)
-library(purrr)
-library(mgx2r)
+## ---- warning = FALSE, message = FALSE, eval=FALSE-----------------------
+#  library(colorRamps)
+#  library(dplyr)
+#  library(plotly)
+#  library(purrr)
+#  library(mgx2r)
+#  
+#  library(cellviz3d)
 
-## ---- warning = FALSE, message = FALSE-----------------------------------
-ply.dir <- system.file("extdata", "normalMesh/", package = "mgx2r")
+## ---- warning = FALSE, message = FALSE, eval=FALSE-----------------------
+#  ply.dir <- system.file("extdata", "full/normalMesh/", package = "mgx2r")
+#  
+#  mesh.all <- map(list.files(ply.dir, recursive = TRUE, full.names = TRUE),
+#                  ~ read_mgxPly(file = .x, ShowSpecimen = FALSE))
+#  
+#  
+#  graph.dir <- system.file("extdata", "full/cellGraph/", package = "mgx2r")
+#  
+#  cellGraph.all <- map(list.files(graph.dir, recursive = TRUE, full.names = TRUE), ~read_mgxCellGraph(fileCellGraph = .x, header_max = 30))
 
-mesh.all <- map(list.files(ply.dir, recursive = TRUE, full.names = TRUE),
-                ~ read_mgxPly(file = .x, ShowSpecimen = FALSE))
+## ---- warning = FALSE, message = FALSE, eval=FALSE-----------------------
+#  meshColors.all <- list(NULL, NULL, NULL, NULL, NULL)
+#  
+#  plotlyMesh_all(meshExample = mesh.all,
+#                         graphExample = cellGraph.all,
+#                         meshColors = meshColors.all,
+#                         display = 'heatmap')
+#  
 
-
-graph.dir <- system.file("extdata", "cellGraph/", package = "mgx2r")
-
-cellGraph.all <- map(list.files(graph.dir, recursive = TRUE, full.names = TRUE), ~read_mgxCellGraph(fileCellGraph = .x, header_max = 30))
-
-## ---- warning = FALSE, message = FALSE-----------------------------------
-meshColors.all <- list(NULL, NULL, NULL, NULL, NULL)
-
-plotlyMesh_all(meshExample = mesh.all,
-                       graphExample = cellGraph.all,
-                       meshColors = meshColors.all,
-                       display = 'heatmap')
-
+## ----echo=FALSE----------------------------------------------------------
+knitr::include_graphics(system.file("img", "full/timeserie800ms.gif", package = "mgx2r"))
 
